ecb8e16415fe1bc88648ea4e52ab7691f09dc482
joeboy77 <acheampongjoseph470@gmail.com>
2024-11-20 15:45:36 +0000

    update

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db/connection');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors')
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
